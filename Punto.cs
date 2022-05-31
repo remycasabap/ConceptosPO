6b2334b3e1d4741a07d57cde5b84688e1907aeed
remycasabap <102169632+remycasabap@users.noreply.github.com>
2022-05-31 12:42:17 -0500

    Create Punto.cs

---
+++ Punto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptosPO
+{
+    class Punto
+    {
+
+        public Punto(int x, int y)
+        {
+            Console.WriteLine($"Coordenada x:  {x},Coordenada y:  {y}");        
+                      
+        }
+
+
+        public Punto()
+        {
+            Console.WriteLine("Este es el constructor por defecto");
+
+        }
+
+    }
+}
